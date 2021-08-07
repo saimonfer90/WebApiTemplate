f4f399f86beb8938aeb6420e9c6e3fc9547b0b2d
Saimon <saimonfer90prog@gmail.com>
2021-08-07 14:10:30 +0200

    Web API Template in .net core base (no deps oltre quelle base)

---
+++ WebApiTemplate/Controllers/WebApiTemplateController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTemplate.Core;
+
+namespace WebApiTemplate.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebApiTemplateController : ControllerBase
+    {
+        private WebApiTemplateCore _core;
+
+        public WebApiTemplateController()
+        {
+            _core = new();
+        }
+
+        [HttpPost]
+        [Route("TryPost")]
+        public async Task<object> GetSomeData([FromBody] string text)
+            => await _core.GetSomeData(text);
+
+        [HttpGet]
+        [Route("TryGet")]
+        public async Task<IEnumerable<int>> GetSomeData()
+            => await _core.GetSomeData();
+    }
+}
---
+++ WebApiTemplate/Core/WebApiTemplateCore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTemplate.Core
+{
+    public class WebApiTemplateCore
+    {
+        public async Task<string> GetSomeData([FromBody] string text)
+        {
+            return "Passed value: " + text;
+        }
+
+        public async Task<IEnumerable<int>> GetSomeData()
+        {
+            return Enumerable.Range(1, 5);
+        }
+    }
+}
